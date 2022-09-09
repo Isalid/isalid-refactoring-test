18909158c6096b067c087787e66806d36ade4eb6
Jérôme L <jerome@jerlio.fr>
2022-09-09 23:42:23 +0200

    Simplify QuoteShortcodeReplacer

--- src/Shortcode/QuoteShortcodeReplacer.php
+++ src/Shortcode/QuoteShortcodeReplacer.php
@@ -4,51 +4,41 @@ namespace Isalid\Shortcode;
 
 use Isalid\Entity\Quote;
 use Isalid\Repository\DestinationRepository;
-use Isalid\Repository\QuoteRepository;
 use Isalid\Repository\SiteRepository;
 
 class QuoteShortcodeReplacer implements ShortcodeReplacer
 {
+    const SHORTCODE_DESTINATION_NAME = '[quote:destination_name]';
+    const SHORTCODE_DESTINATION_LINK = '[quote:destination_link]';
+    const SHORTCODE_SUMMARY = '[quote:summary]';
+    const SHORTCODE_SUMMARY_HTML = '[quote:summary_html]';
+
     public function replace($text, array $data = [])
     {
         $quote = (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
 
-        if ($quote) {
-            $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
-            $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
-            $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
-
-            if (strpos($text, '[quote:destination_link]') !== false) {
-                $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
-            }
-
-            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-            $containsSummary = strpos($text, '[quote:summary]');
-
-            if ($containsSummaryHtml !== false || $containsSummary !== false) {
-                if ($containsSummaryHtml !== false) {
-                    $text = str_replace(
-                        '[quote:summary_html]',
-                        Quote::renderHtml($_quoteFromRepository),
-                        $text
-                    );
-                }
-                if ($containsSummary !== false) {
-                    $text = str_replace(
-                        '[quote:summary]',
-                        Quote::renderText($_quoteFromRepository),
-                        $text
-                    );
-                }
-            }
-
-            (strpos($text, '[quote:destination_name]') !== false) and $text = str_replace('[quote:destination_name]', $destinationOfQuote->countryName, $text);
+        if (!isset($quote)) {
+            return $text;
         }
 
-        if (isset($destination))
-            $text = str_replace('[quote:destination_link]', $usefulObject->url . '/' . $destination->countryName . '/quote/' . $_quoteFromRepository->id, $text);
-        else
-            $text = str_replace('[quote:destination_link]', '', $text);
+        $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
+
+        if (strpos($text, self::SHORTCODE_DESTINATION_NAME) !== false) {
+            $text = str_replace(self::SHORTCODE_DESTINATION_NAME, $destination->countryName, $text);
+        }
+
+        if (strpos($text, self::SHORTCODE_DESTINATION_LINK) !== false) {
+            $site = SiteRepository::getInstance()->getById($quote->siteId);
+            $text = str_replace(self::SHORTCODE_DESTINATION_LINK, $site->url . '/' . $destination->countryName . '/quote/' . $quote->id, $text);
+        }
+
+        if (strpos($text, self::SHORTCODE_SUMMARY) !== false) {
+            $text = str_replace(self::SHORTCODE_SUMMARY, Quote::renderText($quote), $text);
+        }
+
+        if (strpos($text, self::SHORTCODE_SUMMARY_HTML) !== false) {
+            $text = str_replace(self::SHORTCODE_SUMMARY_HTML, Quote::renderHtml($quote), $text);
+        }
 
         return $text;
     }
